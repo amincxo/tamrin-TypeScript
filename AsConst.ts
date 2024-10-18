f5a9ab8a4fd5795bc0fa987698e7db4a6bf0c759
unknown <amincxo@gmail.com>
2024-10-18 14:24:57 +0330

    as const

---
+++ AsConst.ts
@@ -0,0 +1,10 @@
+//As Const
+
+const user :object = {
+    fullName: 'aminborvayeh',
+    age: 26,
+    email: "amin@gmail",
+    password:"asdfasdfa"
+}as const;
+const numers:[number , number , number] =[1 , 5 , 6 ];
+console.log(numers[0])
